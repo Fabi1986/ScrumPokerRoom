f60985dee8bd9c691a78a3db1368b8384d883b96
Fabian <Fabian@Dev>
2020-11-28 16:41:47 +0100

    Set frontend as base url.

--- scrum_poker_room/scrum_poker_room/urls.py
+++ scrum_poker_room/scrum_poker_room/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('frontend/', include('frontend.urls'))
+    path('api', include('api.urls')),
+    path('', include('frontend.urls'))
 ]
